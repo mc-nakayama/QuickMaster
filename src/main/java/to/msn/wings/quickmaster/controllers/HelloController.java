ec60816c39c3a5b32d9c680d586d5c62e96b19b5
mc-nakayama <naka.demy.school@gmail.com>
2021-09-04 20:36:40 +0900

    controllerによる出力

--- src/main/java/to/msn/wings/quickmaster/controllers/HelloController.java
+++ src/main/java/to/msn/wings/quickmaster/controllers/HelloController.java
@@ -1,5 +1,19 @@
 package to.msn.wings.quickmaster.controllers;
 
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+//a.コントローラクラスを定義
+@Controller
+// ↑コントローラの場合はこちらを設定
+public class HelloController{
+//b.Handler(処理)クラスを定義
+	@GetMapping("//hello")
+  // ↑ルーティングの設定
+	@ResponseBody
+	public String index() {
+//c.戻り値はレスポンス本体
+		return "こんにちは、世界!";
+	}
 }
