3660eeb9f0b36e5bd7e1e1f5744523f903c2c0b2
mc-nakayama <naka.demy.school@gmail.com>
2021-09-09 16:57:13 +0900

    greet.html

--- src/main/java/to/msn/wings/quickmaster/controllers/HelloController.java
+++ src/main/java/to/msn/wings/quickmaster/controllers/HelloController.java
@@ -3,6 +3,7 @@ package to.msn.wings.quickmaster.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 //a.コントローラクラスを定義
 @Controller
@@ -16,4 +17,13 @@ public class HelloController{
 //c.戻り値はレスポンス本体
 		return "こんにちは、世界!";
 	}
+  @GetMapping("/greet")
+  //a.Modelオブジェクトの準備
+  public String greet(Model model){
+    //b.ビュー変数の設定
+    model.addAttribute("message","こんにちは、世界！");
+    //c.テンプレートの呼び出し
+    return "greet";
+  }
 }
+
