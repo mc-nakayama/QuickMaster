2d4d5a3ae9261c2ddd2a28a0bc057b99f19db47e
mc-nakayama <naka.demy.school@gmail.com>
2021-10-06 15:59:00 +0900

    Bookに関するコントローラ

--- src/main/java/to/msn/wings/quickmaster/controllers/HelloController.java
+++ src/main/java/to/msn/wings/quickmaster/controllers/HelloController.java
@@ -4,12 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import to.msn.wings.quickmaster.models.BookRepository;
 
 //a.コントローラクラスを定義
 @Controller
 // ↑コントローラの場合はこちらを設定
 public class HelloController{
-//b.Handler(処理)クラスを定義
+  //リポジトリを格納するためのプライベートフィールド
+  private BookRepository rep;
+  //a.コンストラクターにリポジトリに注入
+  @Autowired
+  public HelloController(BookRepository rep){
+    this.rep = rep;
+  }
+  //b.Handler(処理)クラスを定義
+  @GetMapping("/list")
+  public String list(Model model){
+    model.addAttribute("books",rep.findAll());
+    return "list";
+  }
+  //書籍一覧を生成するためのHandlerメソッド
 	@GetMapping("//hello")
   // ↑ルーティングの設定
 	@ResponseBody
